6fb5231f04b6a91679a73eff4a7e85f88b2514dc
Daniel Byler <dbyler@mindresearch.org>
2019-05-08 15:45:54 -0400

    test

--- src/js/components/directives/schedule/event-tile.js
+++ src/js/components/directives/schedule/event-tile.js
@@ -1,9 +1,9 @@
 angular
   .module('msProgram')
-  .directive('eventTile', ['angularLoad', function(angularLoad) {
+  .directive('eventTile', ['angularLoad', function (angularLoad) {
     return {
       restrict: 'A',
-      link: function(scope, element) {
+      link: function (scope, element) {
         var type = scope.session.type;
 
         switch (type) {
@@ -31,57 +31,57 @@ angular
 
         if (type !== 'lunch' && (type !== 'breakout' || type !== 'professor')) {
 
-          element.on('click', function(e) {
-            e.preventDefault();
-            var modal = $('#event-modal');
-            modal.find('.modal-title').html(scope.session.title);
-            modal.find('.modal-time').html('<b>Time: </b>' + '<span>' + scope.session.time + '</span>');
-            modal.find('.modal-speaker').html('<b>Speaker(s):</b> ' + '<span>' + scope.session.organizer + '</span>');
-            modal.find('.modal-location').html('<b>Location:</b> ' + '<span>' + scope.session.location + '</span>');
-            modal.find('.modal-description').html('<b>Description:</b> ' + '<span>' + scope.session.description + '</span>');
-            modal.find('.modal-footer').html(
-              '<div title="Add to Calendar" class="addthisevent" data-direct="outlook">' +
-              'Add to Calendar' +
-              '<span class="start">' + scope.session.start + '</span>' +
-              '<span class="end">' + scope.session.end + '</span>' +
-              '<span class="timezone">' + scope.session.timezone + '</span>' +
-              '<span class="title">' + scope.session.title + '</span>' +
-              '<span class="description">' + scope.session.description + '</span>' +
-              '<span class="location">' + scope.session.location + '</span>' +
-              '<span class="organizer">' + scope.session.organizer + '</span>' +
-              '<span class="date_format">MM/DD/YYYY</span>' +
-              '<span class="_alarm_reminder">15</span>' +
-              '</div>'
-            );
-            modal.find('p span').each(function(i) {
-              $(this).parent().removeClass('hide');
-              if ($(this).html() === "") {
-                $(this).parent().addClass('hide');
-              }
-            });
+          // element.on('click', function(e) {
+          //   e.preventDefault();
+          //   var modal = $('#event-modal');
+          //   modal.find('.modal-title').html(scope.session.title);
+          //   modal.find('.modal-time').html('<b>Time: </b>' + '<span>' + scope.session.time + '</span>');
+          //   modal.find('.modal-speaker').html('<b>Speaker(s):</b> ' + '<span>' + scope.session.organizer + '</span>');
+          //   modal.find('.modal-location').html('<b>Location:</b> ' + '<span>' + scope.session.location + '</span>');
+          //   modal.find('.modal-description').html('<b>Description:</b> ' + '<span>' + scope.session.description + '</span>');
+          //   modal.find('.modal-footer').html(
+          //     '<div title="Add to Calendar" class="addthisevent" data-direct="outlook">' +
+          //     'Add to Calendar' +
+          //     '<span class="start">' + scope.session.start + '</span>' +
+          //     '<span class="end">' + scope.session.end + '</span>' +
+          //     '<span class="timezone">' + scope.session.timezone + '</span>' +
+          //     '<span class="title">' + scope.session.title + '</span>' +
+          //     '<span class="description">' + scope.session.description + '</span>' +
+          //     '<span class="location">' + scope.session.location + '</span>' +
+          //     '<span class="organizer">' + scope.session.organizer + '</span>' +
+          //     '<span class="date_format">MM/DD/YYYY</span>' +
+          //     '<span class="_alarm_reminder">15</span>' +
+          //     '</div>'
+          //   );
+          //   modal.find('p span').each(function(i) {
+          //     $(this).parent().removeClass('hide');
+          //     if ($(this).html() === "") {
+          //       $(this).parent().addClass('hide');
+          //     }
+          //   });
 
-            var script = $('script[src="https://addthisevent.com/libs/1.6.0/ate.min.js"]');
+          //   var script = $('script[src="https://addthisevent.com/libs/1.6.0/ate.min.js"]');
 
-            if (script.length > 0) {
-              addthisevent.refresh();
-            } else {
-              angularLoad.loadScript('https://addthisevent.com/libs/1.6.0/ate.min.js').then(function() {
-                addthisevent.settings({
-                  license: "aen768rfszu2rrgu4mpg5584",
-                  mouse: false,
-                  css: false
-                });
-              }).catch(function() {
-                // If error run this code
-              });
-            }
+          //   if (script.length > 0) {
+          //     addthisevent.refresh();
+          //   } else {
+          //     angularLoad.loadScript('https://addthisevent.com/libs/1.6.0/ate.min.js').then(function() {
+          //       addthisevent.settings({
+          //         license: "aen768rfszu2rrgu4mpg5584",
+          //         mouse: false,
+          //         css: false
+          //       });
+          //     }).catch(function() {
+          //       // If error run this code
+          //     });
+          //   }
 
-            $('#event-modal').modal('show');
+          //   $('#event-modal').modal('show');
 
-            $('.directions').on('click', function() {
-              $('#event-modal').modal('hide');
-            });
-          });
+          //   $('.directions').on('click', function() {
+          //     $('#event-modal').modal('hide');
+          //   });
+          // });
 
         }
       }
